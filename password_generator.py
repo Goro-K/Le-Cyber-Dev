c639edc1556e4d1d817e4fb652aba4fb778bb916
Goro-K <romankizilt@gmail.com>
2024-04-23 17:02:25 +0200

    add comments and delete useless method

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,4 @@
-# Description: Générateur de mot de passe
-# un mot de passe de plus de 15 caractères
+# Description: Générateur de mot de passe aléatoire
 # au moins une lettre majuscule
 # au moins une lettre minuscule
 # au moins un chiffre
@@ -20,8 +19,10 @@ letters = sys.argv[2]
 digits = sys.argv[3]
 special = sys.argv[4]
 
+
 characterList = []
 
+# on ajoute les caractères selon les types choisis
 if letters == 'on':
     characterList += string.ascii_letters
 if digits == 'on':
@@ -33,8 +34,11 @@ if not characterList:
     print("Il faut choisir au moins un type de caractère")
     exit(1)
 
+
 for char in range(length):
-    randomChar = ''.join(secrets.choice(characterList))
+    # on choisit un caractère aléatoire
+    randomChar = secrets.choice(characterList)
+    # on ajoute le caractère aléatoire à la liste
     password.append(randomChar)
 
 print("The random password is " + "".join(password))
