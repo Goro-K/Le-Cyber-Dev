49a776867658c9ce80b5a1a2ab0acb118cf1a614
Goro-K <romankizilt@gmail.com>
2024-04-24 19:42:52 +0200

    add function and css class

--- password_page.php
+++ password_page.php
@@ -1,43 +1,46 @@
 <?php require 'header.php' ?>
 
-<?php
-if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['password_length'])) {
-    $password_length = $_POST['password_length'];
-    $command = "python3 ./password_generator.py ";
-    $command .= escapeshellarg($password_length) . " ";
-    $command .= isset($_POST['letters']) ? 'on ' : 'off ';
-    $command .= isset($_POST['numbers']) ? 'on ' : 'off ';
-    $command .= isset($_POST['special_characters']) ? 'on ' : 'off ';
-    $output = [];
-    $return_var = 0;
-    exec($command, $output, $return_var);
+<?php require 'function python script.php';
 
-    if($return_var === 0) {
-        $password = htmlspecialchars($output[0]);
-    } else {
-        $password = "Veuillez ajouter une taille de mot de passe valide et cocher au moins une option.";
-    }
-}
+$password_length = $_POST['password_length'];
+$letters = isset($_POST['letters']) ? 'on' : 'off';
+$numbers = isset($_POST['numbers']) ? 'on' : 'off';
+$special_characters = isset($_POST['special_characters']) ? 'on' : 'off';
+$error = "Veuillez ajouter une taille de mot de passe valide et cocher au moins une option.";
+
+$data = escapeshellarg($password_length) . " " . escapeshellarg($letters) . " " . escapeshellarg($numbers) . " " . escapeshellarg($special_characters);
+
+// Appel de la fonction python_script
+$password = python_script("password_length", "password_generator", $data, $error);
 ?>
 
-<section>
-    <h2>Veuillez choisir la taille de votre mot de passe.
-        <form action="password_page.php" method="POST">
-            <label for="password_length">Longueur du mot de passe</label>
-            <input type="number" name="password_length" id="password_length" min="8" max="64" required>
-            <label for="letters">Inclure des lettres ?</label>
-            <input type="checkbox" name="letters" id="letters"> 
-            <label for="numbers">Inclure des Numéros ?</label>
-            <input type="checkbox" name="numbers" id="numbers">
-            <label for="special_characters">Inclure des caractères spéciaux ?</label>
-            <input type="checkbox" name="special_characters" id="special_characters">
-            <button type="submit">Générer</button>
-        </form>
-        <?php
-            if (isset($password)) {
-                echo "<p>Le mot de passe est : $password</p>";
-            }
-        ?>
+<section class="form_section">
+    <h2>Veuillez choisir la taille de votre mot de passe.</h2>
+        <div>
+            <form class="password_form" action="password_page.php" method="POST" autocomplete="off">
+                <label for="password_length">Longueur du mot de passe</label>
+                <input type="number" name="password_length" id="password_length" min="8" max="64" required autocomplete="off">
+                <label class="letters" for="letters">Inclure des lettres ?</label>
+                <input class="checkbox" type="checkbox" name="letters" id="letters" autocomplete="off" value="25"> 
+                <label for="numbers">Inclure des Numéros ?</label>
+                <input class="checkbox" type="checkbox" name="numbers" id="numbers" autocomplete="off" value="25">
+                <label for="special_characters">Inclure des caractères spéciaux ?</label>
+                <input class="checkbox" type="checkbox" name="special_characters" id="special_characters" autocomplete="off" value="25">
+                <span class="front" type="submit">Générer ?</button>
+                <span class="back">Done</span>
+                <div class="line">
+                    <div class="fill"></div>
+                </div>
+            </form>
+            <?php
+                if (isset($password) && !empty($password)) {
+                    echo "<p>Le mot de passe est : $password</p>";
+                }
+            ?>
+            <?php require 'todo.php' ?>
+        </div>
 </section>
-</h2>
+
 <?php require 'footer.php' ?>
+
+<script src="line.js"></script>
